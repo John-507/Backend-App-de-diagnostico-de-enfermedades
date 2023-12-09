4d4373da282de8a623dafc291869c688d577924e
John-507 <jambzz53@gmail.com>
2023-12-09 12:59:55 -0500

    Implementación del motor de conocimiento diagnostico y reglas de diagnóstico

--- backend.py
+++ backend.py
@@ -14,3 +14,27 @@ class Sintoma(Fact):
 id_a_nombre_sintoma = {sintoma['id']: sintoma['nombre'] for sintoma in sintomas_data['sintomas']}
 # Crear un diccionario para mapear nombres de enfermedades a su descripción y precauciones
 enfermedades_info = {enfermedad['nombre']: {'descripcion': enfermedad['descripcion'], 'precauciones': enfermedad['precauciones']} for enfermedad in enfermedades_data['enfermedades']}
+
+# Crear una instancia de Sintoma para cada síntoma
+sintomas_dict = {sintoma['nombre']: Sintoma(nombre=sintoma['nombre']) for sintoma in sintomas_data['sintomas']}
+
+# Definir una clase para el diagnóstico
+class Diagnostico(KnowledgeEngine):
+    def __init__(self):
+        super().__init__()
+        self.enfermedades_puntaje = {enfermedad['nombre']: 0 for enfermedad in enfermedades_data['enfermedades']}
+
+    @Rule(Sintoma(nombre=MATCH.nombre))
+    def chequear_sintoma(self, nombre):
+        for enfermedad in enfermedades_data['enfermedades']:
+            if nombre in [id_a_nombre_sintoma[sintoma_id] for sintoma_id in enfermedad['sintomas']]:
+                self.enfermedades_puntaje[enfermedad['nombre']] += 1
+
+    def finalizar_diagnostico(self):
+        enfermedad_sugerida = max(self.enfermedades_puntaje, key=self.enfermedades_puntaje.get)
+        info = enfermedades_info[enfermedad_sugerida]
+        return {
+            "enfermedad": enfermedad_sugerida,
+            "descripcion": info['descripcion'],
+            "precauciones": info['precauciones']
+        }
