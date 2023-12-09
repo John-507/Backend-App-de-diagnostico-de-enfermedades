8111fa91c2f693bf81d31d1acf26422ad3a365b1
SrJeisonA <aguilarjeison15@gmail.com>
2023-12-09 11:37:29 -0500

    Definición de la clase Sintoma y mapeo de IDs a nombres de síntomas.

--- backend.py
+++ backend.py
@@ -1,7 +1,16 @@
 import json
+from experta import Fact, KnowledgeEngine, Rule, MATCH 
+
 # Cargar los datos de síntomas y enfermedades desde los archivos JSON
 with open('sintomas.json', 'r') as file:
  sintomas_data = json.load(file)
 with open('enfermedades.json', 'r') as file:
  enfermedades_data = json.load(file)
- 
+
+# Crear una clase para los síntomas como hechos
+class Sintoma(Fact):
+ pass
+# Crear un diccionario para mapear IDs de síntomas a nombres
+id_a_nombre_sintoma = {sintoma['id']: sintoma['nombre'] for sintoma in sintomas_data['sintomas']}
+# Crear un diccionario para mapear nombres de enfermedades a su descripción y precauciones
+enfermedades_info = {enfermedad['nombre']: {'descripcion': enfermedad['descripcion'], 'precauciones': enfermedad['precauciones']} for enfermedad in enfermedades_data['enfermedades']}
