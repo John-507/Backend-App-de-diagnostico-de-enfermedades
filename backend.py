bb16990c04674926b57c4a5404a4f5b8a6ef23f3
John-507 <jambzz53@gmail.com>
2023-12-09 23:00:02 -0500

    Se añade flask CORS para habilitar todas las rutas de servidor flask

--- backend.py
+++ backend.py
@@ -1,8 +1,10 @@
 import json
 from experta import Fact, KnowledgeEngine, Rule, MATCH 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Cargar los datos de síntomas y enfermedades desde los archivos JSON
 with open('sintomas.json', 'r') as file:
