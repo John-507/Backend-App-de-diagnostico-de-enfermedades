cbca15987b19d3e66c47020a1c69317d95d255af
John-507 <jambzz53@gmail.com>
2023-12-13 11:27:35 -0500

    Se arregla importación de json para que devuelva datos actualizados

--- backend.py
+++ backend.py
@@ -6,38 +6,29 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Cargar los datos de síntomas y enfermedades desde los archivos JSON
-with open('sintomas.json', 'r') as file:
- sintomas_data = json.load(file)
-with open('enfermedades.json', 'r') as file:
- enfermedades_data = json.load(file)
-
 # Crear una clase para los síntomas como hechos
 class Sintoma(Fact):
  pass
-# Crear un diccionario para mapear IDs de síntomas a nombres
-id_a_nombre_sintoma = {sintoma['id']: sintoma['nombre'] for sintoma in sintomas_data['sintomas']}
-# Crear un diccionario para mapear nombres de enfermedades a su descripción y precauciones
-enfermedades_info = {enfermedad['nombre']: {'descripcion': enfermedad['descripcion'], 'precauciones': enfermedad['precauciones']} for enfermedad in enfermedades_data['enfermedades']}
-
-# Crear una instancia de Sintoma para cada síntoma
-sintomas_dict = {sintoma['nombre']: Sintoma(nombre=sintoma['nombre']) for sintoma in sintomas_data['sintomas']}
 
 # Definir una clase para el diagnóstico
 class Diagnostico(KnowledgeEngine):
-    def __init__(self):
+    def __init__(self, enfermedades_data, sintomas_data):
         super().__init__()
+        self.enfermedades_data = enfermedades_data
+        self.sintomas_data = sintomas_data
         self.enfermedades_puntaje = {enfermedad['nombre']: 0 for enfermedad in enfermedades_data['enfermedades']}
+        self.enfermedades_info = {enfermedad['nombre']: {'descripcion': enfermedad['descripcion'], 'precauciones': enfermedad['precauciones']} for enfermedad in enfermedades_data['enfermedades']}
+        self.id_a_nombre_sintoma = {sintoma['id']: sintoma['nombre'] for sintoma in sintomas_data['sintomas']}
 
     @Rule(Sintoma(nombre=MATCH.nombre))
     def chequear_sintoma(self, nombre):
-        for enfermedad in enfermedades_data['enfermedades']:
-            if nombre in [id_a_nombre_sintoma[sintoma_id] for sintoma_id in enfermedad['sintomas']]:
+        for enfermedad in self.enfermedades_data['enfermedades']:
+            if nombre in [self.id_a_nombre_sintoma[sintoma_id] for sintoma_id in enfermedad['sintomas']]:
                 self.enfermedades_puntaje[enfermedad['nombre']] += 1
 
     def finalizar_diagnostico(self):
         enfermedad_sugerida = max(self.enfermedades_puntaje, key=self.enfermedades_puntaje.get)
-        info = enfermedades_info[enfermedad_sugerida]
+        info = self.enfermedades_info[enfermedad_sugerida]
         return {
             "enfermedad": enfermedad_sugerida,
             "descripcion": info['descripcion'],
@@ -49,8 +40,14 @@ def diagnosticar():
     data = request.json
     sintomas_usuario = data['sintomas']
 
+    # Cargar los datos de enfermedades y síntomas desde los archivos JSON
+    with open('enfermedades.json', 'r') as file:
+        enfermedades_data = json.load(file)
+    with open('sintomas.json', 'r') as file:
+        sintomas_data = json.load(file)
+
     # Crear una instancia del motor de conocimiento
-    engine = Diagnostico()
+    engine = Diagnostico(enfermedades_data, sintomas_data)
 
     # Declarar los síntomas seleccionados por el usuario
     engine.reset()
@@ -66,8 +63,14 @@ def diagnosticar():
 
 @app.route('/sintomas', methods=['GET'])
 def obtener_sintomas():
-    # Devuelve la lista de síntomas
+    with open('sintomas.json', 'r') as file:
+        sintomas_data = json.load(file)
     return jsonify(sintomas_data['sintomas'])
+
+
+@app.route('/')
+def index():
+    return "<h1 style='font-size:24px;'>Hola! La Aplicación Flask está en ejecución</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
